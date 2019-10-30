4f78b44a4b027dc6e36749f2501920f10dd153b6
Glen Maddern <glenmaddern@gmail.com>
2019-10-30 23:49:07 +0000

    well would you look at that, tree-shaking was giving me a false positive that my paths were right

--- src/server/fab-server.js
+++ src/server/fab-server.js
@@ -10,6 +10,7 @@ import {
   radioAndTvDataRegexPath,
 } from '../app/routes/regex';
 import getRouteProps from '../app/routes/getInitialData/utils/getRouteProps';
+console.log({routes})
 
 export async function route(settings, path, request) {
   // must return something, by default returns the path unchanged
